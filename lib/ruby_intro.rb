c14e291cd48c775e16dc222d414b1f6e0cc7734d
Nasser Issa <109549903+habibiplease@users.noreply.github.com>
2026-04-13 14:59:29 -0500

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,58 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.reduce(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+
+  sorted = arr.sort
+  sorted[-1] + sorted[-2]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.length < 2
+
+  seen = {}
+  arr.each do |x|
+    return true if seen.key?(n - x)
+    seen[x] = true
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !!(/\A[bcdfghjklmnpqrstvwxyz]/i.match(s))
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless /\A[01]+\z/.match?(s)
+  return true if /\A0+\z/.match?(s)
+
+  s.end_with?('00')
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn == '' || price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', @price)
+  end
 end
